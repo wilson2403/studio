391695a49b1cfd3186a905a80d72c57abea5c58d
wilson Alfaro <wilson2403@gmail.com>
2025-07-20 23:00:46 +0000

    nada aun

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -5,7 +5,7 @@ export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("relative", className)}>
       <Image 
-        src="/__ps-images__/e0775d71-50e5-4f46-817c-0b3f87309995.png" 
+        src="/images/logo.png" 
         alt="El Arte de Sanar Logo" 
         width={100}
         height={100}
