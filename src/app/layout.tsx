d63f187b5efea0628816a172f097372e7f7d7b59
wilson Alfaro <wilson2403@gmail.com>
2025-07-21 04:32:51 +0000

    centro todo en un safe area

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,11 @@ export default function RootLayout({
             >
               <div className="relative flex min-h-screen flex-col">
                 <Header />
-                <main className="flex-1">{children}</main>
+                <main className="flex-1">
+                  <div className="container">
+                    {children}
+                  </div>
+                </main>
                 <Footer />
                 <Chatbot />
               </div>
