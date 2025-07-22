8a45893cbc3a9dd658131957ec9cda597e9f441d
wilson Alfaro <wilson2403@gmail.com>
2025-07-22 15:20:57 +0000

    que los videos de proximas ceremonias tenga un padding de 5 a cada lado

--- src/components/home/Ceremonies.tsx
+++ src/components/home/Ceremonies.tsx
@@ -140,7 +140,7 @@ export default function Ceremonies({
                   <Expand className="h-4 w-4" />
                 </Button>
               </div>
-              <div className="aspect-[9/16] h-[440px] overflow-hidden rounded-t-2xl relative group/video">
+              <div className="aspect-[9/16] h-[440px] overflow-hidden rounded-t-2xl relative group/video px-5">
                  <VideoPlayer 
                     videoUrl={ceremony.mediaUrl} 
                     mediaType={ceremony.mediaType}
@@ -350,6 +350,7 @@ interface CeremoniesProps {
     
 
     
+
 
 
 
