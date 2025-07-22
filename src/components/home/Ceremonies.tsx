3700199e2a37b4dbd21d7c8f44fd306201c1d5cf
wilson Alfaro <wilson2403@gmail.com>
2025-07-22 15:12:49 +0000

    los videos de eventos anteriores hagalos un 20% mas grandes

--- src/components/home/Ceremonies.tsx
+++ src/components/home/Ceremonies.tsx
@@ -176,7 +176,7 @@ export default function Ceremonies({
             >
                 <CarouselContent className="-ml-2">
                 {ceremonies.map((ceremony) => (
-                    <CarouselItem key={ceremony.id} className="basis-1/2 md:basis-1/3 lg:basis-1/4 p-0 pl-2">
+                    <CarouselItem key={ceremony.id} className="basis-full sm:basis-1/2 lg:basis-1/3 p-0 pl-2">
                       <div className="p-1 h-full">
                         <div className="relative rounded-2xl overflow-hidden aspect-[9/16] group/item shadow-2xl shadow-primary/20 border-2 border-primary/30 h-full">
                           {isAdmin && (
@@ -346,5 +346,7 @@ interface CeremoniesProps {
     subtitleId?: string;
     subtitleInitialValue?: string;
 }
+
+    
 
     
