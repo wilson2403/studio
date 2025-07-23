50627412e86598295a950fd8e0bf268b4b7dab2f
wilson Alfaro <wilson2403@gmail.com>
2025-07-23 21:31:47 +0000

    cambia la version por la 1.12

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -38,7 +38,7 @@ import EditProfileDialog from '../auth/EditProfileDialog';
 import { ScrollArea } from '../ui/scroll-area';
 
 const ADMIN_EMAIL = 'wilson2403@gmail.com';
-const APP_VERSION = '1.11';
+const APP_VERSION = '1.12';
 
 export default function Header() {
   const pathname = usePathname();
