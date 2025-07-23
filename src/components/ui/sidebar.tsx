c2bea78de333b82f79345f0b92e66743f1a9a3c4
wilson Alfaro <wilson2403@gmail.com>
2025-07-23 19:24:17 +0000

    El panel administrativo tiene un scrol a al derecha

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -222,15 +222,11 @@ const Sidebar = React.forwardRef<
         data-variant={variant}
         data-side={side}
       >
-        {/* This is what handles the sidebar gap on desktop */}
         <div
           className={cn(
-            "hidden md:block duration-200 relative h-svh w-[--sidebar-width] bg-transparent transition-[width] ease-linear",
+            "hidden md:block duration-200 relative h-svh w-0 bg-transparent transition-[width] ease-linear",
             "group-data-[collapsible=offcanvas]:w-0",
             "group-data-[side=right]:rotate-180",
-            variant === "floating" || variant === "inset"
-              ? "group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)_+_theme(spacing.4))]"
-              : "group-data-[collapsible=icon]:w-[--sidebar-width-icon]"
           )}
         />
         <div
@@ -307,6 +303,7 @@ const SidebarRail = React.forwardRef<
         "group-data-[collapsible=offcanvas]:translate-x-0 group-data-[collapsible=offcanvas]:after:left-full group-data-[collapsible=offcanvas]:hover:bg-sidebar",
         "[[data-side=left][data-collapsible=offcanvas]_&]:-right-2",
         "[[data-side=right][data-collapsible=offcanvas]_&]:-left-2",
+        "hidden",
         className
       )}
       {...props}
