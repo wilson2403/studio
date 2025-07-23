0954e6cd564bc7b152f34870ca04e210f23ed039
wilson Alfaro <wilson2403@gmail.com>
2025-07-23 21:51:53 +0000

    en la seccion de ceremonias en el video dice statusActive

--- src/app/ceremonies/page.tsx
+++ src/app/ceremonies/page.tsx
@@ -307,3 +307,6 @@ export default function AllCeremoniesPage() {
         </EditableProvider>
     );
 }
+
+
+    
--- src/components/home/Ceremonies.tsx
+++ src/components/home/Ceremonies.tsx
@@ -425,3 +425,5 @@ interface CeremoniesProps {
     subtitleInitialValue?: string;
 }
 
+
+    
