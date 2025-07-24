f07d1b521b2fb65e0e87d3a237da78e7dcc544be
wilson Alfaro <wilson2403@gmail.com>
2025-07-24 15:42:38 +0000

    <meta name="google-site-verification" content="3ikN5R9KIadbh6RorKv3LtApl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
   description:
     'Descubre cómo la Ayahuasca puede transformar tu vida. Únete a nuestras ceremonias guiadas por expertos para una sanación profunda y una renovada conexión espiritual.',
   keywords: 'ayahuasca, ceremonia de ayahuasca, medicina ancestral, sanación espiritual, retiro espiritual, el arte de sanar',
+  other: {
+    'google-site-verification': '3ikN5R9KIadbh6RorKv3LtApl6c-M0tEqmdqHuuN_Wk',
+  },
 };
 
 export default function RootLayout({
