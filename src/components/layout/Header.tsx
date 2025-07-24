adeb74e3819148ec6d4e31e1d26ed13a7a969213
wilson Alfaro <wilson2403@gmail.com>
2025-07-24 23:32:58 +0000

    cambiale la version 1.39

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -38,7 +38,7 @@ import EditProfileDialog from '../auth/EditProfileDialog';
 import { ScrollArea } from '../ui/scroll-area';
 
 const ADMIN_EMAIL = 'wilson2403@gmail.com';
-const APP_VERSION = '1.38';
+const APP_VERSION = '1.39';
 
 export default function Header() {
   const pathname = usePathname();
