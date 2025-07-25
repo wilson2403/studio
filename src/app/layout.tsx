17ecd687d1794b8804b54123d2d8847e53457938
wilson Alfaro <wilson2403@gmail.com>
2025-07-25 01:27:00 +0000

    En el <title> del HTML pon algo como:
    Ceremonias de Ayahuasca en Costa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,10 @@ import { EditableProvider } from '@/components/home/EditableProvider';
 import LoadingScreen from '@/components/layout/LoadingScreen';
 
 export const metadata: Metadata = {
-  title: 'El Arte de Sanar - Un Encuentro Sagrado con Medicinas Ancestrales',
+  title: 'Ceremonias de Ayahuasca en Costa Rica - El Arte de Sanar',
   description:
-    'Descubre cómo la Ayahuasca puede transformar tu vida. Únete a nuestras ceremonias guiadas por expertos para una sanación profunda y una renovada conexión espiritual.',
-  keywords: 'ayahuasca, ceremonia de ayahuasca, medicina ancestral, sanación espiritual, retiro espiritual, el arte de sanar',
+    'Ofrecemos ceremonias de ayahuasca con enfoque terapéutico, espiritual y ancestral en Costa Rica. Transformación interior y conexión con la medicina.',
+  keywords: 'ayahuasca, ceremonia de ayahuasca, medicina ancestral, sanación espiritual, retiro espiritual, el arte de sanar, costa rica',
   other: {
     'google-site-verification': '3ikN5R9KIadbh6RorKv3LtApl6c-M0tEqmdqHuuN_Wk',
   },
