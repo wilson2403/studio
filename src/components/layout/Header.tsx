19ebb9bf2739c7ddbb2a27cae07f402894b500d5
wilson Alfaro <wilson2403@gmail.com>
2025-07-28 23:19:28 +0000

    1.60

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -38,7 +38,7 @@ import EditProfileDialog from '../auth/EditProfileDialog';
 import { ScrollArea } from '../ui/scroll-area';
 import { Badge } from '../ui/badge';
 
-const APP_VERSION = '1.59';
+const APP_VERSION = '1.60';
 
 export default function Header() {
   const pathname = usePathname();
