9143eb3567802bcd2141ad2ab9751511922b7fc0
wilson Alfaro <wilson2403@gmail.com>
2025-07-29 03:19:06 +0000

    1.68

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -40,7 +40,7 @@ import { Badge } from '../ui/badge';
 import { SystemSettings } from '@/types';
 import { getSystemSettings } from '@/ai/flows/settings-flow';
 
-const APP_VERSION = '1.67';
+const APP_VERSION = '1.68';
 
 type NavLinkDef = {
     href: string;
