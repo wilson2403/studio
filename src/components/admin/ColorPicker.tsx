dbab2309d269831cdd9bc698e07670b2e0d60e82
wilson2403 <wilson2403@hotmail.com>
2025-09-09 22:11:38 +0000

    Try fixing this error: `Runtime Error: Error: Button is not defined. Err

--- src/components/admin/ColorPicker.tsx
+++ src/components/admin/ColorPicker.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { cn } from '@/lib/utils';
 import { Paintbrush } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 const colors = [
     // Original Greens & Golds
