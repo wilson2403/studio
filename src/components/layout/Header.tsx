bb30937efe7abcdc65a2185e87f61ee3a293604c
Wilson <wilson2403@gmail.com>
2025-09-16 02:33:20 +0000

    version 3.10

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -42,7 +42,7 @@ import { getSystemSettings } from '@/ai/flows/settings-flow';
 import { useAuth } from '@/hooks/useAuth';
 import TutorialDialog from './TutorialDialog';
 
-const APP_VERSION = '3.9';
+const APP_VERSION = '3.10';
 const ADMIN_EMAILS = ['wilson2403@gmail.com', 'wilson2403@hotmail.com'];
 
 type NavLinkDef = {
