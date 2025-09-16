ee8042d76f585503915d2d95b8a26507c0513e2c
Wilson <wilson2403@gmail.com>
2025-09-16 17:21:53 +0000

    el de /interpreter/en no carga a ingles

--- src/app/interpreter/[[...lang]]/client.tsx
+++ src/app/interpreter/[[...lang]]/client.tsx
@@ -11,13 +11,12 @@ export default function InterpreterClientPage({ lang }: { lang?: string }) {
     // and then the dialog will be triggered by the pathname in DreamInterpreterDialog.
     const languageToSet = lang === 'en' ? 'en' : 'es';
     if (i18n.language !== languageToSet) {
-      i18n.changeLanguage(languageToSet);
+      i18n.changeLanguage(languageToSet).then(() => {
+          // Force a reload to apply the new language resources everywhere
+          window.location.reload();
+      });
     }
   }, [lang, i18n]);
 
   return null; // This page doesn't render any visible content.
 }
-
-    
-
-    
